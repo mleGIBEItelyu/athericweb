c12a45352c62b1a5c8e6c57bc4c1d179a4f01010
theepar <threpasty@gmail.com>
2026-07-20 00:40:34 +0800

    security: sanitize markdown rendering in AiChatPanel to prevent XSS

--- src/components/dashboard/AiChatPanel.tsx
+++ src/components/dashboard/AiChatPanel.tsx
@@ -10,7 +10,14 @@ const QUICK_PROMPTS = [
 ]
 
 function renderMarkdown(text: string): string {
-  return text
+  const escaped = text
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+
+  return escaped
     .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
     .replace(/\*(.+?)\*/g, '<em>$1</em>')
     .replace(/^• (.+)$/gm, '<li>$1</li>')
